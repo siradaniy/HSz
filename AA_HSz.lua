2d2796b3c1b9ce404e1d17f5267760f6927f8708
siradaniy <83637146+siradaniy@users.noreply.github.com>
2023-01-18 17:12:22 +0700

    Update AA_HSz.lua

--- AA_HSz.lua
+++ AA_HSz.lua
@@ -218,7 +218,7 @@ function sex()
 
     local autofrmserver = win:Server("Auto Farm Section", "http://www.roblox.com/asset/?id=11579310982")
     local webhookserver = win:Server("Discord Wehhook  ", "http://www.roblox.com/asset/?id=11585480207")
-	local diskordserver = win:Server("Discord Server   ", "http://www.roblox.com/asset/?id=12155612675")
+    local diskordserver = win:Server("Discord Server   ", "http://www.roblox.com/asset/?id=12155612675")
     local creditsserver = win:Server("Credits          ", "http://www.roblox.com/asset/?id=11579371312")
 
 

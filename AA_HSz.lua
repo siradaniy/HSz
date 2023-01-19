b3172912a2cdc1d2d106e95d1a31cc68326acabf
siradaniy <83637146+siradaniy@users.noreply.github.com>
2023-01-20 03:18:28 +0700

    Update AA_HSz.lua

--- AA_HSz.lua
+++ AA_HSz.lua
@@ -34,7 +34,7 @@ local function webhook()
     	XP = tostring(game:GetService("Players").LocalPlayer.PlayerGui.ResultsUI.Holder.LevelRewards.ScrollingFrame.XPReward.Main.Amount.Text)
 		gems = tostring(game:GetService("Players").LocalPlayer.PlayerGui.ResultsUI.Holder.LevelRewards.ScrollingFrame.GemReward.Main.Amount.Text)
 		gold = tostring(game:GetService("Players").LocalPlayer.PlayerGui.ResultsUI.Holder.LevelRewards.ScrollingFrame.GoldReward.Main.Amount.Text)
-		item = tostring(game:GetService("ReplicatedStorage").endpoints.server_to_client.show_unit_and_item_rewards)
+		item = tostring(game:GetService("ReplicatedStorage").endpoints.server_to_client.show_unit_and_item_rewards.text)
 		cwaves = game:GetService("Players").LocalPlayer.PlayerGui.ResultsUI.Holder.Middle.WavesCompleted.Text
 		ctime = game:GetService("Players").LocalPlayer.PlayerGui.ResultsUI.Holder.Middle.Timer.Text
 		waves = cwaves:split(": ")

6c630013bc61a7216c8da46236105c149ab544bd
siradaniy <83637146+siradaniy@users.noreply.github.com>
2023-01-30 01:29:22 +0700

    Update AA_HSz.lua

--- AA_HSz.lua
+++ AA_HSz.lua
@@ -19,7 +19,7 @@ local RunService = game:GetService("RunService")
 local mouse = game.Players.LocalPlayer:GetMouse()
 local UserInputService = game:GetService("UserInputService")
 
-getgenv().savefilename = "Anime-Adventures_HSz_UPD10_"..game.Players.LocalPlayer.Name..".json"
+getgenv().savefilename = "Anime-Adventures_HSz_UPD10_fix"..game.Players.LocalPlayer.Name..".json"
 getgenv().door = "_lobbytemplategreen1"
 
 ------------item drop result

22da651aa3e09613754fc61a015b595a64871460
siradaniy <83637146+siradaniy@users.noreply.github.com>
2023-02-22 19:41:14 +0700

    Update AA_HSz_V2.lua

--- AA_HSz_V2.lua
+++ AA_HSz_V2.lua
@@ -1846,7 +1846,7 @@ coroutine.resume(coroutine.create(function()
                     local a={[1]="next_story"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
                     local a={[1]="next_story"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
                     print("Next Story...")
-                elseif getgenv().AutoContinue then
+                elseif Settings.AutoContinue then
                     local a={[1]="NextRetry"} game:GetService("ReplicatedStorage").endpoints.client_to_server.request_start_infinite_tower_from_game:InvokeServer(unpack(a))
                     local a={[1]="NextRetry"} game:GetService("ReplicatedStorage").endpoints.client_to_server.request_start_infinite_tower_from_game:InvokeServer(unpack(a))   
                     print("Next Room infint Castle...")     

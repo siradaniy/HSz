8555b871f9cdb58b3f6b8f94117150ad47931fe8
siradaniy <83637146+siradaniy@users.noreply.github.com>
2023-02-26 16:19:01 +0700

    Update AA_HSz_V2.lua

--- AA_HSz_V2.lua
+++ AA_HSz_V2.lua
@@ -170,7 +170,7 @@ function webhook()
           ["fields"] = {
              {
               ["name"] ="ผลรวม",
-              ["value"] = " Map name : "..mapname.. " 🗺️\nMap Id : "..world.. " 🗺️\nผลต่อสู้ : "..result.. " ⚔️\nWave ที่จบ : " ..tostring(waves[2]).."<:wave:997136622363627530>\n เวลาที่ใช้ : " ..tostring(ttime[2]).." ⌛\n ",
+              ["value"] = " โหมด name : "..mapname.. " 🗺️\nMap name : "..world.. " 🗺️\nผลต่อสู้ : "..result.. " ⚔️\nWave ที่จบ : " ..tostring(waves[2]).."<:wave:997136622363627530>\n เวลาที่ใช้ : " ..tostring(ttime[2]).." ⌛\n ",
               ["inline"] = true
             },
             {

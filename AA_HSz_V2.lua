14f477e7ece6d16f9346fdc971a04b10edb471b1
siradaniy <83637146+siradaniy@users.noreply.github.com>
2023-02-28 00:17:22 +0700

    Update AA_HSz_V2.lua

--- AA_HSz_V2.lua
+++ AA_HSz_V2.lua
@@ -176,7 +176,7 @@ function webhook()
             },
             {
                ["name"] ="Results :",
-               ["value"] = " ```ini\nWorld name : "..mapname.. " 🗺️\nMap name : "..world.. " 🗺️\nผลต่อสู้ : "..result.. " ⚔️\nWave ที่จบ : " ..tostring(waves[2]).."🌊\nเวลาที่ใช้ : " ..tostring(ttime[2]).." ⌛\n ```",
+               ["value"] = " ```ini\n🌏 World name : "..mapname.. "\n🗺️ Map name : "..world.. "\n⚔️ ผลต่อสู้ : "..result.. "\n🌊 Wave ที่จบ : " ..tostring(waves[2]).."\n⌛ เวลาที่ใช้ : " ..tostring(ttime[2]).."\n ```",
                ["inline"] = true
             },
             {

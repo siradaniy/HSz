2e3f9fd4a77e664009d5886a9e1ab719c8e11dcd
siradaniy <83637146+siradaniy@users.noreply.github.com>
2023-03-04 23:04:21 +0700

    Update AA_HSz_Beta_V2.lua

--- AA_HSz_Beta_V2.lua
+++ AA_HSz_Beta_V2.lua
@@ -537,7 +537,7 @@ local function WorldSec()
         if level == "namek_infinite" or level == "aot_infinite" or level == "demonslayer_infinite" 
         or level == "naruto_infinite" or level == "marineford_infinite" or level == "tokyoghoul_infinite" or level == "hueco_infinite" 
         or level == "hxhant_infinite" or level == "magnolia_infinite" or level == "jjk_infinite" or level == "clover_infinite" 
-        or level == "jojo_infinite" or level == "opm_infinite" or cata == "Legend Stages" or cata == "Raid Worlds" or cata == "Dungeon"  then
+        or level == "jojo_infinite" or level == "opm_infinite" or level == "7ds_infinite" or cata == "Legend Stages" or cata == "Raid Worlds" or cata == "Dungeon"  then
             diff = {"Hard"}
         elseif cata == "Portals" then
             diff = {"Default"}

83fcd631da18475acd6975872b137a2882b9e46e
siradaniy <83637146+siradaniy@users.noreply.github.com>
2023-04-05 04:34:24 +0700

    Update SaveManager.lua

--- SaveManager.lua
+++ SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-	SaveManager.Folder = 'LinoriaLibSettings'
+	SaveManager.Folder = 'V3_Anime Adventures_Settings'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
